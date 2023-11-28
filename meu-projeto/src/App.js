5a64b6e03227c8a0a7d2f27db6606a9691089769
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-11-28 15:15:43 -0300

    Continuação curso React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+  const name = 'Henrique'
+  const newName = name.toUpperCase()
+  function sum(a, b){
+    return a + b
+  }
+  const url = "https://via.placeholder.com/150"
   return (
     <div className="App">
         <h1>Olá React!</h1>
         <p>Meu primeiro App!</p>
+        <p>Olá, {newName}!</p>
+        <p>Soma: {sum(1, 2)}</p>
+        <img src={url} alt= 'Minha imagem'/>
     </div>
   );
 }
