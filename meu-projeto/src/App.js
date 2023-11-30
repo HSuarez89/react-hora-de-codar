ad46182ee2d30acfa481974ea0a4c105c9321d86
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-11-30 08:27:00 -0300

    Props no React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import './components/HelloWorld.js';
 import HelloWorld from './components/HelloWorld';
 import Frase from './components/Frase.js';
+import SayMyName from './components/SayMyName.js';
+import Pessoa from './components/Pessoa.js';
+import PessoaEstruturado from './components/PessoaEstruturado.js';
 
 function App() {
   const name = 'Henrique'
   const newName = name.toUpperCase()
+  const nome = "José"
   function sum(a, b){
     return a + b
   }
@@ -17,7 +21,23 @@ function App() {
         <p>Olá, {newName}!</p>
         <p>Soma: {sum(1, 2)}</p>
         <img src={url} alt= 'Minha imagem'/>
+        <Frase />
         <HelloWorld />
+        <h1>Props</h1>
+        <SayMyName nome="Henrique" />
+        <SayMyName nome='Hugo' />
+        <SayMyName nome={nome} />
+        <Pessoa nome="Henrique"
+          idade="34"
+          profissao="Programador"
+          foto="https://via.placeholder.com/150"
+        />
+        <PessoaEstruturado
+          nome="Yete Suarez Siqueira"
+          idade="67"
+          profissao="Dentista"
+          foto="https://via.placeholder.com/150"
+        />
     </div>
   );
 }
