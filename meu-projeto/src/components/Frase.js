15b67f66db0826317ddc1f86097981034f75dc52
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-11-30 14:32:53 -0300

    Inserindo CSS no React

--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from './Frase.module.css'
+
 function Frase(){
     return(
-        <div>
-            <p>Esta frase é um compontente!</p>
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>Esta frase é um compontente!</p>
         </div>
     )
 }
