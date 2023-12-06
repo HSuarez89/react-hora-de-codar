eb2438f61bcdcecd052e9ed01123247237b7bfe7
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-12-06 15:32:03 -0300

    react icons

--- meu-projeto2/src/components/layout/Footer.js
+++ meu-projeto2/src/components/layout/Footer.js
@@ -1,7 +1,16 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer(){
     return(
         <footer>
             <h2>Rodapé da página</h2>
+            <ul className={styles.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+            </ul>
+            <p>Nosso rodapé</p>
         </footer>
     )
 }
