040f22b16bde438960f3c6b4ce8ebd0915561d42
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-12-14 11:42:57 -0300

    Criando api

--- costs/src/components/pages/NovoProjeto.js
+++ costs/src/components/pages/NovoProjeto.js
@@ -1,12 +1,28 @@
+import {useNavigate} from 'react-router-dom'
 import FormularioProjeto from '../projetos/FormularioProjeto'
 import styles from './NovoProjeto.module.css'
 
 function NovoProjeto(){
+    const navigate = useNavigate()
+    function criarProjeto(projeto){
+        // inicializando custos e serviços
+        projeto.cost = 0
+        projeto.servicos = []
+
+        fetch('http://localhost:5000/projetos', {
+            method: "POST",
+            headers: {'content-Type': 'application.json'},
+            body: JSON.stringify(projeto)
+        }).then((resp) => resp.json())
+        .then((data) => console.log(data),
+        navigate('/projetos', {mensagem: 'Projeto criado com sucesso!'}))
+        .catch((err) => console.log(err))
+    }
     return (
         <div className={styles.novoprojeto_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <FormularioProjeto btnText='Criar Projeto'/>
+            <FormularioProjeto handleSubmit={criarProjeto} btnText='Criar Projeto'/>
         </div>
     )
 }
--- costs/src/components/projetos/FormularioProjeto.js
+++ costs/src/components/projetos/FormularioProjeto.js
@@ -4,8 +4,9 @@ import Input from '../formulario/Input'
 import Select from '../formulario/Select'
 import styles from './FormularioProjeto.module.css'
 
-function FormularioProjeto({btnText}){
+function FormularioProjeto({handleSubmit, btnText, dadosProjeto}){
     const [categoria, setCategoria]= useState([])
+    const [projeto, setProjeto] = useState(dadosProjeto || {})
 
     useEffect(() =>{
         fetch('http://localhost:5000/categorias', {
@@ -18,11 +19,28 @@ function FormularioProjeto({btnText}){
     .then((data) => {setCategoria(data)})
     .catch((err) => console.log(err))
     }, [])
+
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(projeto)
+    }
+
+    function handleChange(e){
+        setProjeto({ ...projeto, [e.target.name]: e.target.value})
+    }
+
+    function handleCategoria(e){
+        setProjeto({ ...projeto, categoria: {
+            id: e.target.value,
+            nome: e.target.options[e.target.selectedIndex].text,
+        },})
+    }
+
     return (
-        <form className={styles.formulario}>
-            <Input type='text' text='Nome do projeto' name='name' placeholder='Insira o nome do projeto' />
-            <Input type='number' text='Orçamento do projeto' name='orçamento' placeholder='Insira o orçamento total' />
-            <Select name='id_categoria' text='Selecione a categoria' options={categoria}/>
+        <form onSubmit={submit} className={styles.formulario}>
+            <Input type='text' text='Nome do projeto' name='name' placeholder='Insira o nome do projeto' handleOnChange={handleChange} value={projeto.name ? projeto.name : ''}/>
+            <Input type='number' text='Orçamento do projeto' name='orcamento' placeholder='Insira o orçamento total' handleOnChange={handleChange} value={projeto.orcamento ? projeto.orcamento : ''}/>
+            <Select name='id_categoria' text='Selecione a categoria' options={categoria} handleOnChange={handleCategoria} value={projeto.categoria ? projeto.categoria.id : ''}/>
             <Botao text={btnText}/>
         </form>
     )
