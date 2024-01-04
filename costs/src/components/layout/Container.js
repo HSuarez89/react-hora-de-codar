abc3b9310b2a96f1ea20bf84fa4a96550a080ec3
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2024-01-03 22:06:12 -0300

    Atualizando layout

--- costs/src/components/layout/Container.js
+++ costs/src/components/layout/Container.js
@@ -2,8 +2,10 @@ import styles from './Container.module.css'
 import { Outlet } from 'react-router-dom'
 
 function Container(props){
-    return (<div className={`${styles.container} ${styles[props.customClass]}`}>{props.children}
-    <Outlet />
+    return (
+    <div className={`${styles.container} ${styles[props.customClass]}`}>
+        {props.children}
+        <Outlet />
     </div>
     )
 }
